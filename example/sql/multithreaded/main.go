587ab8315621ec80e2a4bfb31f5d2ef1e88b9a3d
trantaiBD <trantai@bytedance.com>
2023-09-07 10:48:17 +0800

    upd README.md + examples for multi-threading usage

---
+++ example/sql/multithreaded/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	bytehouse "github.com/bytehouse-cloud/driver-go"
+	_ "github.com/bytehouse-cloud/driver-go/sql" //this is required, otherwise "bytehouse" driver will not be registered
+)
+
+func main() {
+	host := "<<hostname>>"
+	port := "<<port>>"
+	apiToken := "<<YOUR_API_TOKEN>>"
+
+	dsn := fmt.Sprintf("tcp://%s:%s?secure=true&user=bytehouse&password=%s", host, port, apiToken)
+	queryString := "select * from numbers(100)"
+	pool, err := sql.Open("bytehouse", dsn)
+	if err != nil {
+		fmt.Printf("error = %v", err)
+		return
+	}
+	defer pool.Close()
+
+	//setup the database configuration here
+	pool.SetMaxOpenConns(10)
+	pool.SetConnMaxIdleTime(time.Minute)
+	pool.SetConnMaxLifetime(time.Minute * 5)
+	pool.SetMaxIdleConns(10)
+
+	threadCount := 10
+	eg := errgroup.Group{}
+
+	for i := 0; i < threadCount; i++ {
+		i := i
+		eg.Go(func() error {
+			threadID := fmt.Sprintf("thread-%v", i)
+			//we need to create bytehouse.NewQueryContext() to assign a query ID and add query settings
+			queryCtx := bytehouse.NewQueryContext(context.Background())
+			//set the query ID here, duplicate query IDs will be rejected
+			queryCtx.SetQueryID(fmt.Sprintf("my_query_id%v", time.Now().String()))
+			if err := queryCtx.AddQuerySetting("max_block_size", "2000"); err != nil {
+				log.Fatalf("thread %v failed to add query setting err = %v", threadID, err)
+				return err
+			}
+
+			if err := pool.Ping(); err != nil {
+				log.Fatalf("thread %v failed to ping err = %v", threadID, err)
+				return err
+			}
+			rows, err := pool.QueryContext(queryCtx, queryString)
+			if err != nil {
+				log.Fatalf("thread %v failed to query err = %v", threadID, err)
+				return err
+			}
+
+			//we must close the row
+			defer rows.Close()
+			var num int64
+
+			for rows.Next() {
+				if err := rows.Scan(&num); err != nil {
+					log.Fatalf("thread %v failed to scan row err = %v", threadID, err)
+					return err
+				}
+
+				fmt.Printf("\nID:%v Value: %v", threadID, num)
+			}
+			fmt.Printf("\n%v Done!", threadID)
+			return nil
+		})
+	}
+
+	//wait for all threads to finish
+	if err := eg.Wait(); err != nil {
+		log.Fatalf("one of the threads have an error:= %v", err)
+	}
+
+	fmt.Println("\nAll threads done!")
+}
---
+++ example/sql/simple/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	bytehouse "github.com/bytehouse-cloud/driver-go"
+	_ "github.com/bytehouse-cloud/driver-go/sql" //this is required, otherwise "bytehouse" driver will not be registered
+)
+
+func main() {
+
+	host := "<<hostname>>"
+	port := "<<port>>"
+	apiToken := "<<YOUR_API_TOKEN>>"
+
+	dsn := fmt.Sprintf("tcp://%s:%s?secure=true&user=bytehouse&password=%s", host, port, apiToken)
+
+	db, err := sql.Open("bytehouse", dsn)
+	if err != nil {
+		fmt.Printf("error = %v", err)
+		return
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("failed to ping, error = %v", err)
+	}
+	ctx := context.Background()
+	queryCtx := bytehouse.NewQueryContext(ctx)
+	queryCtx.SetQueryID("my_query_id")
+	if err := queryCtx.AddQuerySetting("max_block_size", "2000"); err != nil {
+		log.Fatalf("failed to add query setting, error = %v", err)
+	}
+
+	rows, err := db.QueryContext(queryCtx, "select * from numbers(100)")
+	if err != nil {
+		log.Fatalf("failed to query err = %v", err)
+	}
+
+	//must close the row
+	defer rows.Close()
+	var num int64
+
+	rowNum := 1
+	for rows.Next() {
+		if err := rows.Scan(&num); err != nil {
+			log.Fatalf("failed to scan row error = %v", err)
+		}
+
+		fmt.Printf("\nRow %v: %v", rowNum, num)
+		rowNum++
+	}
+
+}
